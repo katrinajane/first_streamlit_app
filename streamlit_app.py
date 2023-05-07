ddd2d6b8f8fbc717457f47a1c935469c1a2d5813
katrinajane <132664028+katrinajane@users.noreply.github.com>
2023-05-08 01:11:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ streamlit.write('The user entered ', fruit_choice)
 #streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 #import snowflake.connector
 
